40491e0bb8ec7657e255249b1c38403123f33023
Smirthi <70279572+smirthi21112000@users.noreply.github.com>
2022-11-23 16:11:21 +0530

    Create password.sh
    Bash script password.sh

---
+++ Docker-image/password.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p ~/docker-registry/auth
+mkdir -p ~/docker-registry/data
+cd ~/docker-registry/auth
+echo 'Give username for your private docker registry'
+read username
+sleep 3
+htpasswd -Bc registry.password $username
