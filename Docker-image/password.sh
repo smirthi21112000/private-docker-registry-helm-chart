9b4d688deb11270760bf52dd17acd2f0ac8c73cd
Smirthi <70279572+smirthi21112000@users.noreply.github.com>
2022-11-24 15:16:10 +0530

    Update password.sh

--- Docker-image/password.sh
+++ Docker-image/password.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-mkdir -p ~/docker-registry/auth
-mkdir -p ~/docker-registry/data
-cd ~/docker-registry/auth
+mkdir -p /docker-registry/auth
+mkdir -p /docker-registry/data
+cd docker-registry
+chown k8suser auth/
+cd auth
+
 echo 'Give username for your private docker registry'
 read username
-sleep 3
+sleep 1
 htpasswd -Bc registry.password $username
+
